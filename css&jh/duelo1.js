8ab61f5145e02b45a5526bcc93da667c53b24a7b
FENIX23z <marcossaullizanojaca@gmail.com>
2025-09-23 15:34:04 +0200

    Add files via upload

---
+++ css&jh/duelo1.js
@@ -0,0 +1,250 @@
+const girlsImages = [
+    "https://i.imgur.com/4BCN8b1.png",
+    "https://i.imgur.com/8YYnBRs.png",
+    "https://imgur.com/aubnOEo.png",
+    "https://imgur.com/Xxh0YI6.png",
+    "https://imgur.com/ri6fAp4.png",
+    "https://i.imgur.com/3RE9xYz.jpeg",
+    "https://i.imgur.com/7xnNazv.png",
+    "https://i.imgur.com/lwTndDL.png",
+    "https://i.imgur.com/LxdgFzh.png",
+    "https://i.imgur.com/hTTDc1D.gif",
+    "https://i.imgur.com/tW5UjQr.png",
+    "https://i.imgur.com/SEUpszW.png",
+    "https://i.imgur.com/40hBB2Y.png",
+    "https://i.imgur.com/O4bP9tF.png",
+    "https://i.imgur.com/kQJ0xTg.png",
+    "https://i.imgur.com/SAVYPji.png",
+    "https://i.imgur.com/V98fejZ.png",
+    "https://i.imgur.com/A7HSQAk.png",
+    "https://i.imgur.com/7nXZLsO.png",
+    "https://i.imgur.com/viZX8yh.png",   
+    "https://i.imgur.com/z4uRxGb.png",
+    "https://i.imgur.com/f4aIZ6i.png",
+    "https://i.imgur.com/0ee90Ph.png",
+    "https://i.imgur.com/Cf7nyQG.png",
+    "https://i.imgur.com/PAJ8ZHd.png",
+    "https://i.imgur.com/5DArabk.png",
+    "https://i.imgur.com/xWhzOUC.png",
+    "https://i.imgur.com/QQaufn7.png",
+    "https://i.imgur.com/pO7Uk3O.png",
+    "https://i.imgur.com/Pun97cJ.png",
+    "https://i.imgur.com/xoEy7Yu.png",
+    "https://i.imgur.com/ztVfHVU.png",
+    "https://i.imgur.com/im1X0zZ.png",
+    "https://i.imgur.com/md70Efd.png",
+    "https://i.imgur.com/kTDmf82.png",
+    "https://i.imgur.com/7IQPhk9.png",
+    "https://i.imgur.com/aQ8NvhS.png",
+    "https://i.imgur.com/OF8KYao.png",
+    "https://i.imgur.com/dxYeZI9.png",
+    "https://i.imgur.com/NUJpaE2.png",
+    "https://i.imgur.com/iwr19BE.png",
+    "https://i.imgur.com/9gw755p.png",
+    "https://i.imgur.com/CmBXPWS.png",
+    "https://i.imgur.com/vMGNBQ6.png",
+    "https://i.imgur.com/q8rIczl.png",
+    "https://i.imgur.com/6Q9bsEt.png",
+    "https://i.imgur.com/jvOWye8.png",
+    "https://i.imgur.com/2OSOIp8.png",
+    "https://i.imgur.com/ry1guCO.png",
+    "https://i.imgur.com/i5Jrg3H.png",
+    "https://i.imgur.com/1kadLHd.png",
+    "https://i.imgur.com/aburEUK.png",
+];
+
+const manImages = [
+    "https://imgur.com/QmnEI0f.png",
+    "https://imgur.com/mgHVT8C.png",
+    "https://imgur.com/Hy5SN7v.png",
+    "https://imgur.com/wTujA1m.png",
+    "https://imgur.com/KGqbsYA.png",
+    "https://i.imgur.com/ck8wKSt.png",
+    "https://i.imgur.com/TPLOSsh.png",
+    "https://i.imgur.com/R9NLEzD.png",
+    "https://i.imgur.com/yr7wCR0.png",
+    "https://i.imgur.com/xugvQY9.png",
+    "https://i.imgur.com/kVD14n6.png",
+    "https://i.imgur.com/lqR8uLE.png",
+    "https://i.imgur.com/mxPMx3v.png",
+    "https://i.imgur.com/TsDchdE.png",
+    "https://i.imgur.com/ocvpL4b.jpeg",
+    "https://i.imgur.com/eThxtfE.png",
+    "https://i.imgur.com/tBIvhWZ.png",
+    "https://i.imgur.com/D3FjAVe.png",
+    "https://i.imgur.com/J1Wr01J.png",
+    "https://i.imgur.com/M5QVxqz.png",
+    "https://i.imgur.com/Tob4spE.png",
+    "https://i.imgur.com/Emv3NTT.png",
+    "https://i.imgur.com/iToW6nO.png",
+    "https://i.imgur.com/gzIprxN.png",
+    "https://i.imgur.com/kNYnhL2.png",
+    "https://i.imgur.com/ImoReqr.png",
+    "https://i.imgur.com/oZMRnoT.png",
+    "https://i.imgur.com/kozZFQy.png",
+    "https://i.imgur.com/WJAM7Ao.png",
+    "https://i.imgur.com/iQmiMg3.jpeg",
+    "https://i.imgur.com/vAI0KcK.png",
+    "https://i.imgur.com/kfQq5XX.png",
+    "https://i.imgur.com/g4lWABz.png",
+    "https://i.imgur.com/WKvX3tf.png",
+    "https://i.imgur.com/2Pl6ZY9.png",
+    "https://i.imgur.com/qIbINFI.png",
+    "https://i.imgur.com/E7VwZ3s.png",
+    "https://i.imgur.com/O3mXVnL.png",
+    "https://i.imgur.com/8Ye9fiB.png",
+    "https://i.imgur.com/mblmzq2.png",
+    "https://i.imgur.com/brcf4F4.png",
+    "https://i.imgur.com/DQCqyiI.png",
+    "https://i.imgur.com/vnKpVrF.png",
+    "https://i.imgur.com/ZWvUKDc.png",
+    "https://i.imgur.com/RXpKvai.png",
+    "https://i.imgur.com/MAQRx6V.png",
+    "https://i.imgur.com/Vlbmj9P.png",
+    "https://i.imgur.com/lGar4um.png",
+    "https://i.imgur.com/rlRZ8U1.png",
+    "https://i.imgur.com/rwCcFs7.png",
+    "https://i.imgur.com/3NY3W8P.png",
+    "https://i.imgur.com/Ye0clup.png",
+];
+
+let round = 0;
+let score = { Girls: 0, Man: 0 };
+
+function updateScore() {
+    document.getElementById('score').textContent = `Feminas: ${score.Girls} | Hombres: ${score.Man}`;
+    document.getElementById('match-count').textContent = `${round + 1} / ${girlsImages.length}`;
+}
+
+function showRound() {
+    if (round >= girlsImages.length) {
+        declareWinner();
+        return;
+    }
+
+    const battle = document.getElementById('battle');
+    const winnerDiv = document.getElementById('winner');
+    battle.innerHTML = '';
+    winnerDiv.style.display = 'none';
+
+    // Contenedor de imágenes
+    const imagesRow = document.createElement('div');
+    imagesRow.className = 'video-container';
+
+    const container1 = document.createElement('div');
+    container1.className = 'video-item';
+    container1.innerHTML = `
+        <img src="${girlsImages[round]}" alt="Girls">
+        <ul><li><span onclick="choose('Girls')">Seleccionar este</span></li></ul>
+    `;
+
+    const container2 = document.createElement('div');
+    container2.className = 'video-item';
+    container2.innerHTML = `
+        <img src="${manImages[round]}" alt="Man">
+        <ul><li><span onclick="choose('Man')">Seleccionar este</span></li></ul>
+    `;
+
+    imagesRow.appendChild(container1);
+    imagesRow.appendChild(container2);
+
+    // Contenedor del botón de empate
+    const empateContainer = document.createElement('div');
+    empateContainer.className = 'empate-container';
+
+
+    // Añadir todo al campo de batalla
+    battle.appendChild(imagesRow);
+    battle.appendChild(empateContainer);
+
+    updateScore();
+}
+
+function choose(team) {
+    if (team === 'Girls') {
+        score.Girls++;
+    } else if (team === 'Man') {
+        score.Man++;
+    } else if (team === 'Empate') {
+        // 🔹 Suma 1 a cada equipo en empate
+        score.Girls++;
+        score.Man++;
+    }
+    round++;
+    showRound();
+}
+
+function launchConfetti() {
+    const canvas = document.createElement('canvas');
+    canvas.id = 'confetti-canvas';
+    canvas.style.position = 'fixed';
+    canvas.style.top = '0';
+    canvas.style.left = '0';
+    canvas.style.width = '100%';
+    canvas.style.height = '100%';
+    canvas.style.pointerEvents = 'none';
+    document.body.appendChild(canvas);
+
+    const ctx = canvas.getContext('2d');
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    const confettiPieces = [];
+    const colors = ['#FF4500', '#FFA500', '#FFD700', '#FFFFFF'];
+
+    for (let i = 0; i < 100; i++) {
+        confettiPieces.push({
+            x: Math.random() * canvas.width,
+            y: Math.random() * canvas.height - canvas.height,
+            size: Math.random() * 10 + 5,
+            speed: Math.random() * 3 + 2,
+            color: colors[Math.floor(Math.random() * colors.length)],
+            rotation: Math.random() * 360,
+            rotationSpeed: Math.random() * 5 - 2.5
+        });
+    }
+
+    function animateConfetti() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        confettiPieces.forEach((piece, index) => {
+            piece.y += piece.speed;
+            piece.rotation += piece.rotationSpeed;
+
+            ctx.save();
+            ctx.translate(piece.x, piece.y);
+            ctx.rotate(piece.rotation * Math.PI / 180);
+            ctx.fillStyle = piece.color;
+            ctx.fillRect(-piece.size / 2, -piece.size / 2, piece.size, piece.size);
+            ctx.restore();
+
+            if (piece.y > canvas.height) {
+                confettiPieces[index].y = -piece.size;
+                confettiPieces[index].x = Math.random() * canvas.width;
+            }
+        });
+        requestAnimationFrame(animateConfetti);
+    }
+
+    animateConfetti();
+    setTimeout(() => canvas.remove(), 5000);
+}
+
+function declareWinner() {
+    const battle = document.getElementById('battle');
+    const winnerDiv = document.getElementById('winner');
+    battle.innerHTML = '';
+    winnerDiv.style.display = 'flex';
+
+    const winnerText = score.Girls > score.Man ? '¡Feminas GANAN!' :
+                       score.Man > score.Girls ? '¡Machismo GANA!' :
+                       '¡El feminismo gano!';
+
+    winnerDiv.innerHTML = `
+        <h2 class="winner-title">${winnerText}</h2>
+        <p class="winner-score">Girls: ${score.Girls} - Man: ${score.Man}</p>
+    `;
+
+    launchConfetti();
+}
+
+document.addEventListener('DOMContentLoaded', showRound);
